e97d223e839091634fe219c88f45c81bf999ea4d
Amresh2104 <60054130+Ranjan2104@users.noreply.github.com>
2021-06-09 11:18:10 +0530

    Main Files

---
+++ Main.py
@@ -0,0 +1,16 @@
+# Amresh Ranjan.
+
+import pywhatkit as kt
+
+print('Wiki Summery wait few seconds!\n')
+
+target1 = "Python"
+target2 = 'Java'
+
+print('\n')
+kt.info(target1,lines=3)
+
+print('\n')
+kt.info(target2,lines=3)
+
+ 
